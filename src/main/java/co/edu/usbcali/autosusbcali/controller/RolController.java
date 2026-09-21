603d73ef4c148d86f12e5fc60fa793c8d7192a5f
JuanCarvajal28 <jmartincg28@gmail.com>
2026-09-21 10:02:02 -0500

    Rol controller

---
+++ src/main/java/co/edu/usbcali/autosusbcali/controller/RolController.java
@@ -0,0 +1,23 @@
+package co.edu.usbcali.autosusbcali.controller;
+
+import co.edu.usbcali.autosusbcali.domain.Rol;
+import co.edu.usbcali.autosusbcali.repository.RolRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/roles")
+@AllArgsConstructor
+public class RolController {
+    private final RolRepository rolRepository;
+    
+    @GetMapping
+    public List<Rol> obtenerTodos(){
+        return rolRepository.findAll();
+    }
+}
